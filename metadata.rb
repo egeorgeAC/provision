6615b9850c69547c4988d41a768617f5ced5d739
Emmett George <egeorge@activecampaign.com>
2016-02-24 12:47:41 -0600

    Delete metadata.rb

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'provisioning_cookbook'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures provisioning_cookbook'
-long_description 'Installs/Configures provisioning_cookbook'
-version '0.1.0'
